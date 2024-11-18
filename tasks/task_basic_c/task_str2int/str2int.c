025ab299e663e9aaf21948507c116b2690f9a3e1
welut0 <emil.laishev@mail.ru>
2024-11-18 22:53:44 +0300

    str2int 100%

--- tasks/task_basic_c/task_str2int/str2int.c
+++ tasks/task_basic_c/task_str2int/str2int.c
@@ -3,5 +3,23 @@
 #include "stdio.h"
 
 int str2int(const char *str) {
-    return 0;
+    int z = 1;
+    int r = 0;
+
+    if (*str =='-'){
+        z = -1;
+        str++;
+    }
+    else if (*str =='+'){
+        str++;
+    }
+    int c = 0;
+    while (*str != '\0'){
+        r = r * 10 + (*str - '0');
+        c += 1;
+        str++;
+        assert(z == -1 && r == -2147483648 || z == -1 && r >= 0 && c <= 9 || z ==1 && r >= 0);
+    }
+    assert(c > 0);
+    return r * z;
 }
